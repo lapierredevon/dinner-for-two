734f4c85d5535e562ac75f503a08aab2297969a6
Devon Lapierre <lapierredevon@gmail.com>
2023-03-28 14:50:43 -0400

    project-setup

---
+++ src/navigation/header.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+/**
+ * Work on navigation menu bar.
+ * Home
+ * Menu
+ * About
+ * Contact
+ * Reservation
+ */
+export default function Header() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Menu</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Reservation</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
