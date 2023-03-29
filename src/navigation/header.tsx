0c5a3a9f90e09bd93553ba7c1fcdbc771ddd3468
Devon Lapierre <lapierredevon@gmail.com>
2023-03-29 18:53:21 -0400

    fix-flex-properties-navbar

--- src/navigation/header.tsx
+++ src/navigation/header.tsx
@@ -1,24 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 /**
- * Work on navigation menu bar.
- * Home
- * Menu
- * About
- * Contact
- * Reservation
+ * Make the navigation menu bar mobile responsive
+ * When the app is on the phone screen I want it to show 3 lines that when it is clicked o by the user  all of the menu items appear
  */
 export default function Header() {
   return (
-    <header>
+    <header className="bg-slate-600 text-stone-100">
       <nav>
-        <ul>
-          <li>Home</li>
-          <li>Menu</li>
-          <li>About</li>
-          <li>Contact</li>
-          <li>Reservation</li>
-        </ul>
+        <div className="flex justify-end max-w-screen h-[80px]">
+          <div className="flex items-end space-x-4">
+            <Link to={"/home"}>Home</Link>
+            <Link to={"/menu"}>Menu</Link>
+            <Link to={"/about"}>About Us</Link>
+            <Link to={"/reservations"}>Reservations</Link>
+          </div>
+        </div>
       </nav>
     </header>
   );
