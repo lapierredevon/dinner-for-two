79fd1ff7528740532a8f0fe6f7bd29c487f83413
Devon Lapierre <lapierredevon@gmail.com>
2023-04-16 17:20:15 -0400

    fix-menu-section-1

--- src/navigation/Menu.tsx
+++ src/navigation/Menu.tsx
@@ -7,87 +7,54 @@ import menuRoll from "../imagesAndVideos/menuRolls.png";
  * Work on backend
  * use useeffect to load the required data for the page
  * Then add the current styling
- * @returns tsx
+ * @returns tsxcreated_at
  */
 
 export default function Menu() {
-  const [menu, setMenu] = useState([]);
+  interface MenuList {
+    created_at?: string;
+    menu_item: string;
+    price: number;
+    sushi_id: number;
+    updated_at?: string;
+  }
+
+  const [menu, setMenu] = useState<MenuList[]>([]);
 
   useEffect(() => {
     const abortController = new AbortController();
     const loadMenu = async () => {
       let menuItems = await fetch("http://localhost:5001/sushi");
       let displayMenu = await menuItems.json();
-      console.log(displayMenu);
-      setMenu(displayMenu);
-      // console.log(menu);
+      console.log("test", displayMenu);
+      setMenu(displayMenu.data);
     };
 
     loadMenu();
     return () => abortController.abort();
   }, []);
 
+  console.log("test 2", menu);
   return (
-    <div className="bg-emerald-950 text-stone-100 h-screen">
-      <h1 className="text-center md:text-7xl mb-7">Rolls</h1>
+    <div className="bg-emerald-950 text-stone-100 sm:h-screen md:h-fit">
+      <h1 className="text-center md:text-6xl mb-7">Rolls</h1>
       <div>
         <div>
-          <div className="flex justify-evenly">
+          <div className="flex md:justify-evenly ">
             <div className="flex flex-col">
-              <div className="flex md:flex-row space-x-3 md:text-3xl">
-                <p>California Roll </p>
-                <p>$7.99</p>
-              </div>
-              <div className="flex md:flex-row space-x-3 md:text-3xl">
-                <p>California Roll </p>
-                <p>$7.99</p>
-              </div>
-              <div className="flex md:flex-row space-x-3 md:text-3xl">
-                <p>California Roll </p>
-                <p>$7.99</p>
-              </div>
-              <div className="flex md:flex-row space-x-3 md:text-3xl">
-                <p>California Roll </p>
-                <p>$7.99</p>
-              </div>
-              <div className="flex md:flex-row space-x-3 md:text-3xl">
-                <p>California Roll </p>
-                <p>$7.99</p>
-              </div>
-              <div className="flex md:flex-row space-x-3 md:text-3xl">
-                <p>California Roll </p>
-                <p>$7.99</p>
-              </div>
-              <div className="flex md:flex-row space-x-3 md:text-3xl">
-                <p>California Roll </p>
-                <p>$7.99</p>
-              </div>
-              <div className="flex md:flex-row space-x-3 md:text-3xl">
-                <p>California Roll </p>
-                <p>$7.99</p>
-              </div>
-              <div className="flex md:flex-row space-x-3 md:text-3xl">
-                <p>California Roll </p>
-                <p>$7.99</p>
-              </div>
-              <div className="flex md:flex-row space-x-3 md:text-3xl">
-                <p>California Roll </p>
-                <p>$7.99</p>
-              </div>
-              <div className="flex md:flex-row space-x-3 md:text-3xl">
-                <p>California Roll </p>
-                <p>$7.99</p>
-              </div>
-              <div className="flex md:flex-row space-x-3 md:text-3xl">
-                <p>California Roll </p>
-                <p>$7.99</p>
-              </div>
+              {menu !== null &&
+                menu.map((meal) => (
+                  <div
+                    key={meal.sushi_id}
+                    className="flex md:flex-row space-x-3 md:text-3xl"
+                  >
+                    <p>{meal.menu_item}</p>
+                    <p>{`$${meal.price}.00`}</p>
+                  </div>
+                ))}
             </div>
-            <div>
-              <img
-                src={menuRoll}
-                className="sm:h-[200px] sm:w-[300px] md:h-[700px] md:w-[850px] rounded-lg"
-              />
+            <div className="sm:h-fill sm:w-[300px] md:h-[700px] md:w-[850px] rounded-lg">
+              <img src={menuRoll} />
             </div>
           </div>
         </div>
