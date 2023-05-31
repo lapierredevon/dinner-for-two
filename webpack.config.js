2c0527772651441bb366783062397ad5bf1f2ca1
Devon Lapierre <lapierredevon@gmail.com>
2023-05-31 19:22:18 -0400

    web-pack-run-buld

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 module.exports = {
   entry: "./src/index.tsx",
-  mode: "development",
+  mode: "production",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
